98c39f34ee2e7aef0e6f25496c7287feeb212d6c
zjyyyyyyyyyyyy <1345925904@qq.com>
2025-04-18 11:43:41 +0800

    feat: 添加测试代码，验证 AI 审查联动

--- bad_style.js
+++ bad_style.js
@@ -1,12 +1,11 @@
-// bad_style.js
 function fn1(a, b) {
-    let tmp = a + b;
-    let x = tmp * 2;
-    return x;
-  }
-  function fn2(c, d) {
-    let tmp = c + d;
-    let y = tmp * 2;
-    return y;
-  }
+  let tmp = a + b;
+  return tmp;
+}
+
+function fn2(x, y) {
+  let tmp = x + y;
+  return tmp;
+}
+
   

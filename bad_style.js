7f90e10472bebd1518b9e7b4d660c19d7499b6e0
zjyyyyyyyyyyyy <1345925904@qq.com>
2025-04-18 11:17:18 +0800

    feat: 添加测试代码，验证 AI 审查联动

---
+++ bad_style.js
@@ -0,0 +1,12 @@
+// bad_style.js
+function fn1(a, b) {
+    let tmp = a + b;
+    let x = tmp * 2;
+    return x;
+  }
+  function fn2(c, d) {
+    let tmp = c + d;
+    let y = tmp * 2;
+    return y;
+  }
+  

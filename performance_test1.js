070af448cba5b91b45c1b1cb9b30e9a7b491eb08
zjyyyyyyyyyyyy <1345925904@qq.com>
2025-04-18 14:14:08 +0800

    test: 追加内容验证 AI 审查提示

--- performance_test1.js
+++ performance_test1.js
@@ -1,9 +1,1 @@
-function compute(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      for (let k = 0; k < arr.length; k++) {
-        console.log(arr[i], arr[j], arr[k]);
-      }
-    }
-  }
-}
+"console.log('¶îÍâ²âÊÔ');" 
